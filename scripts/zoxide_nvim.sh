09bef52b247be854127b1c51e876c8621075bb94
Fangxing Liu <fxliu@Mac.mshome.net>
2025-04-18 14:58:50 +0200

    update script

--- scripts/zoxide_nvim.sh
+++ scripts/zoxide_nvim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 search_with_zoxide() {
-	if [ -z "$1"]; then
-		# search using fzf when no filename is provided 
-		file="$(fd --type f -I -H -E .git -E .cache -E .backup | fzf --preview='bat -n --color=always --line-range :500 {}')"	
-		if [ -n "$file"]; then
-			nvim "$file"
-		fi
-	else
+        if [ -z "$1" ]; then
+            # use fd with fzf to select & open a file when no arg are provided
+            file="$(fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup | fzf --height=70% --preview='bat -n --color=always --line-range :500 {}')"
+            if [ -n "$file" ]; then
+                nvim "$file"
+            fi
+        else
 		# Handle when an arg is provided
         lines=$(zoxide query -l | xargs -I {} fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup -E .vscode "$1" {} | fzf --no-sort) # Initial filter attempt with fzf
         line_count="$(echo "$lines" | wc -l | xargs)" # Trim any leading spaces
